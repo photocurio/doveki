fdc65dfc62f291782bdf1cbec7fe9210775c69c0
photocurio <sunraydust@gmail.com>
2022-12-30 07:50:30 -0500

    convert to Solid JS

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js'
+
+const App: Component = () => {
+	return (
+		<div>
+			<header>
+				<h1>Dovkie</h1>
+			</header>
+		</div>
+	)
+}
+
+export default App
